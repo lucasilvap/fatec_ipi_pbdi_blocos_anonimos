3b1a3d1502796c00098631392bb33ac760ad5fec
Lucas Silva <silva.pereira.92@gmail.com>
2023-09-05 08:52:54 -0300

    ap08(blocos-anonimos): codigo 2.3.1

---
+++ script.sql
@@ -0,0 +1,11 @@
+DO
+$$
+BEGIN
+	-- exibir uma mensagem
+	-- RAISE NOTICE 'Meu primeiro bloco anônimo';
+	-- placeholders (guardiao de lugar)
+	-- 2 + 2 = 4
+	RAISE NOTICE '% + % = %', 2, 2, 2 + 2;
+END;
+
+$$
